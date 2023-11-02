3ad84b23fc35bbe77f0a5460bce9b4ebce41ce51
Darius Ronkowitz <dronkowitz@yahoo.com>
2023-11-02 15:58:16 -0400

    `Updated main.py
    Fixed finishes touches of game code.
    Completed SolorSystem Game.
    Users can change the number of planets shown on screen.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from math import *
 from tkinter.tix import *
 
 tk = Tk()
-screen = Canvas(tk, width = 600, height = 600, background = "black")
+screen = Canvas(tk, width = 600, height = 600, background = "Black")
 screen.pack()
 
 class planet:
@@ -23,14 +23,14 @@ planetSize = [7, 7, 8, 4, 7, 30]
 orbitSize = [60, 120, 180, 184, 250, 300]
 
 angles = [0, 212, 123, 123, 180, 230]
-orbitingSpeeds = [4.5, 2.5, 1.5, 1.5, 1.7, 0.6]
+orbitingSpeeds = [15, 20, 14, 13, 10, 7]
 
 colors = ["grey90", "hotpink", "red", "orange", "yellow", "green", "cyan3"]
-planetDrawing = [0, 0, 0 , 0, 0, 0, 0]
+planetDrawing = []
 numPlanets = len(orbitSize)
 
 for i in range(numPlanets):
- p = planet(random.randint(8, 20), random.randint(60, 300),random.randint(0, 360), random.uniform(1, 5), colors[random.randint(0, len(colors) - 1)])
+ p = planet(random.randint(8, 20), random.randint(60, 300),random.randint(0, 360), random.uniform(1, 5), colors[random.randint(0, len(colors) -1)])
  planetList.append(p)
  planetDrawing.append(0) 
   
@@ -43,31 +43,38 @@ def drawCircle(xC, yC, r, col):
 
 def drawStars(numStars):
   for i in range(0, numStars):
-    xC = randint(0, 600)
-    yC = randint(0, 600)
-    size = uniform(1, 2)
+    x = random.randint(0, 600)
+    y = random.randint(0, 600)
+    size = random.uniform(1, 2)
     drawCircle(x, y, size, "white")
 
-def drawSun(xC, yC, col, sunRadius):
-  drawCircle(xC. yC, sunRadius, "yellow")
+def drawSun(xC, yC, sunRadius):
+  drawCircle(xC, yC, sunRadius, "yellow")
 
 def drawPlanets(xSun, ySun, degAngle, orbitalRadius, planetSize, color, fx, fy):
   radianAngle = radians(degAngle)
   xPlanet = xSun + orbitalRadius * fx(radianAngle)
-  yPlanet = ySun + orbitalRadius * fy(radianAngle)
+  yPlanet = ySun - orbitalRadius * fy(radianAngle)
   return drawCircle(xPlanet, yPlanet, planetSize, color)
-  
+
 def animateSolarSystem(numStars, xSun, ySun, fx, fy):
   drawStars(numStars)
   
   drawSun(xSun, ySun, 35)
-  while (True):
+  
+  while True:
     for i in range(numPlanets):
-      planetDrawing[i] = drawPlanets(xSun, ySun, angles[i], orbitSize[i], planetSize[i], colors[i], fx, fy)
-      angles[i] = angles[i] + orbitingSpeeds[i]
-      screen.update()
-      sleep(.03)
-      for i in range(numPlanets):
-        screen.delete(planetDrawing[i])
+      planetDrawing[i] = drawPlanets(xSun, ySun,             planetList[i].angle, planetList[i].orbit, planetList[i].size, planetList[i]. color, fx, fy)
+      planetList[i].angle = planetList[i].angle + planetList[i].speed
+    screen.update()
+    sleep(.03)
+    for i in range(numPlanets):
+      screen.delete(planetDrawing[i])
+class window(Frame):
+  def __init__(self, master = None):
+    Frame.__init__ (self, master)
+    self.master = master
+    self.pack(fill = BOTH, expand = 1)
+
 animateSolarSystem(0, 300, 300, sin, cos)
 tk.mainloop()
